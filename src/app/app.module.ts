ac7e38d475a97cf272632c890d9560a1036488af
AIT TGHRAYT Youssef <tghrayt.ysf@gmail.com>
2022-03-07 15:57:11 +0100

    Création des forms login/register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './compenents/login/login.component';
@@ -22,7 +24,8 @@ import { LayoutComponent } from './shared/layout/layout.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
